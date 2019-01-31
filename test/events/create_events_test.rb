134772dc9285bdde18c7c3e027494be362ddfb2f
Ben Verbeken <ben@seats.io>
2019-01-31 11:38:25 +0100

    create multiple events in one go

---
+++ test/events/create_events_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+require 'util'
+require 'seatsio/domain'
+
+class ChartReportsTest < SeatsioTestClient
+
+  def test_multiple_events
+    chart_key = create_test_chart
+    event_creation_params = [{}, {}]
+    events = @seatsio.events.create_multiple(key: chart_key, event_creation_params: event_creation_params)
+
+    assert_equal(2, events.length)
+    events.each {|e| assert_not_nil(e["key"])}
+
+  end
+
+end
