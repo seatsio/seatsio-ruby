cae8d48f44f39129b760b4507adbdead522852d8
mroloux <matti@seats.io>
2019-04-15 21:08:50 +0200

    Fixed tests

--- test/events/event_reports_summary_test.rb
+++ test/events/event_reports_summary_test.rb
@@ -17,12 +17,12 @@ class EventReportsSummaryTest < SeatsioTestClient
     assert_equal(1, report['booked']['byCategoryKey']['9'])
     assert_equal(1, report['booked']['byCategoryLabel']['Cat1'])
 
-    assert_equal(33, report['free']['count'])
-    assert_equal(33, report['free']['bySection']['NO_SECTION'])
-    assert_equal(16, report['free']['byCategoryKey']['9'])
-    assert_equal(17, report['free']['byCategoryKey']['10'])
-    assert_equal(16, report['free']['byCategoryLabel']['Cat1'])
-    assert_equal(17, report['free']['byCategoryLabel']['Cat2'])
+    assert_equal(231, report['free']['count'])
+    assert_equal(231, report['free']['bySection']['NO_SECTION'])
+    assert_equal(115, report['free']['byCategoryKey']['9'])
+    assert_equal(116, report['free']['byCategoryKey']['10'])
+    assert_equal(115, report['free']['byCategoryLabel']['Cat1'])
+    assert_equal(116, report['free']['byCategoryLabel']['Cat2'])
   end
 
   def test_summary_by_category_key
@@ -33,14 +33,14 @@ class EventReportsSummaryTest < SeatsioTestClient
 
     report = @seatsio.event_reports.summary_by_category_key(event.key)
 
-    assert_equal(17, report['9']['count'])
-    assert_equal(17, report['9']['bySection']['NO_SECTION'])
+    assert_equal(116, report['9']['count'])
+    assert_equal(116, report['9']['bySection']['NO_SECTION'])
     assert_equal(1, report['9']['byStatus']['booked'])
-    assert_equal(16, report['9']['byStatus']['free'])
+    assert_equal(115, report['9']['byStatus']['free'])
 
-    assert_equal(17, report['10']['count'])
-    assert_equal(17, report['10']['bySection']['NO_SECTION'])
-    assert_equal(17, report['10']['byStatus']['free'])
+    assert_equal(116, report['10']['count'])
+    assert_equal(116, report['10']['bySection']['NO_SECTION'])
+    assert_equal(116, report['10']['byStatus']['free'])
   end
 
   def test_summary_by_category_label
@@ -51,14 +51,14 @@ class EventReportsSummaryTest < SeatsioTestClient
 
     report = @seatsio.event_reports.summary_by_category_label(event.key)
 
-    assert_equal(17, report['Cat1']['count'])
-    assert_equal(17, report['Cat1']['bySection']['NO_SECTION'])
+    assert_equal(116, report['Cat1']['count'])
+    assert_equal(116, report['Cat1']['bySection']['NO_SECTION'])
     assert_equal(1, report['Cat1']['byStatus']['booked'])
-    assert_equal(16, report['Cat1']['byStatus']['free'])
+    assert_equal(115, report['Cat1']['byStatus']['free'])
 
-    assert_equal(17, report['Cat2']['count'])
-    assert_equal(17, report['Cat2']['bySection']['NO_SECTION'])
-    assert_equal(17, report['Cat2']['byStatus']['free'])
+    assert_equal(116, report['Cat2']['count'])
+    assert_equal(116, report['Cat2']['bySection']['NO_SECTION'])
+    assert_equal(116, report['Cat2']['byStatus']['free'])
   end
 
   def test_summary_by_section
@@ -69,12 +69,12 @@ class EventReportsSummaryTest < SeatsioTestClient
 
     report = @seatsio.event_reports.summary_by_section(event.key)
 
-    assert_equal(34, report['NO_SECTION']['count'])
+    assert_equal(232, report['NO_SECTION']['count'])
     assert_equal(1, report['NO_SECTION']['byStatus']['booked'])
-    assert_equal(33, report['NO_SECTION']['byStatus']['free'])
-    assert_equal(17, report['NO_SECTION']['byCategoryKey']['9'])
-    assert_equal(17, report['NO_SECTION']['byCategoryKey']['10'])
-    assert_equal(17, report['NO_SECTION']['byCategoryLabel']['Cat1'])
-    assert_equal(17, report['NO_SECTION']['byCategoryLabel']['Cat2'])
+    assert_equal(231, report['NO_SECTION']['byStatus']['free'])
+    assert_equal(116, report['NO_SECTION']['byCategoryKey']['9'])
+    assert_equal(116, report['NO_SECTION']['byCategoryKey']['10'])
+    assert_equal(116, report['NO_SECTION']['byCategoryLabel']['Cat1'])
+    assert_equal(116, report['NO_SECTION']['byCategoryLabel']['Cat2'])
   end
 end
