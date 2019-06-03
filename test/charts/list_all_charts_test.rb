ca70749a2a7c547131351a508a1de162faf51707
Nahuel Chaves <nahuel@seats.io>
2019-06-03 14:17:24 +0000

    Add withValidation to chart list

--- test/charts/list_all_charts_test.rb
+++ test/charts/list_all_charts_test.rb
@@ -64,6 +64,28 @@ class ListAllChartsTest < SeatsioTestClient
     assert_equal([event2.id, event1.id], event_ids)
   end
 
+  def test_with_validation
+    5.times do
+      @seatsio.charts.create
+    end
+
+    retrieved_charts = @seatsio.charts.list(with_validation: true).to_a
+    validations = retrieved_charts.collect {|chart| chart.validation}
+
+    assert_equal(validations, [{"errors" => [], "warnings" => []}] * 5)
+  end
+
+  def test_without_validation
+    5.times do
+      @seatsio.charts.create
+    end
+
+    retrieved_charts = @seatsio.charts.list().to_a
+    validations = retrieved_charts.collect {|chart| chart.validation}
+
+    assert_equal(validations, [nil] * 5)
+  end
+
   def test_without_charts
     charts = @seatsio.charts.list.to_a
 
