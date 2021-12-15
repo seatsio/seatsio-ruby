ad02411e974c062228344d9b0ff09936b01c6daa
Ben Verbeken <ben@seats.io>
2021-12-15 17:18:53 +0100

    Support allowed and rejected previous statuses (#41)
    * .gitignored /out
    
    * Support allowed and rejected previous statuses

--- test/events/change_object_status_test.rb
+++ test/events/change_object_status_test.rb
@@ -16,8 +16,8 @@ class ChangeObjectStatusTest < SeatsioTestClient
     a1 = res.objects['A-1']
     assert_equal('status_foo', a1.status)
     assert_equal('A-1', a1.label)
-    assert_equal({'own' => {'label' => '1', 'type' => 'seat'}, 'parent' => {'label' => 'A', 'type' => 'row'}}, a1.labels)
-    assert_equal({'own' => '1', 'parent' => 'A'}, a1.ids)
+    assert_equal({ 'own' => { 'label' => '1', 'type' => 'seat' }, 'parent' => { 'label' => 'A', 'type' => 'row' } }, a1.labels)
+    assert_equal({ 'own' => '1', 'parent' => 'A' }, a1.ids)
     assert_equal('Cat1', a1.category_label)
     assert_equal('9', a1.category_key)
     assert_nil(a1.ticket_type)
@@ -62,8 +62,8 @@ class ChangeObjectStatusTest < SeatsioTestClient
   def test_ticket_type
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
-    props1 = {:objectId => 'A-1', :ticketType => 'Ticket Type 1'}
-    props2 = {:objectId => 'A-2', :ticketType => 'Ticket Type 2'}
+    props1 = { :objectId => 'A-1', :ticketType => 'Ticket Type 1' }
+    props2 = { :objectId => 'A-2', :ticketType => 'Ticket Type 2' }
 
     @seatsio.events.change_object_status(event.key, [props1, props2], 'status_foo')
 
@@ -79,8 +79,8 @@ class ChangeObjectStatusTest < SeatsioTestClient
   def test_quantity
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
-    props1 = {:objectId => 'GA1', :quantity => 5}
-    props2 = {:objectId => 'GA2', :quantity => 10}
+    props1 = { :objectId => 'GA1', :quantity => 5 }
+    props2 = { :objectId => 'GA2', :quantity => 10 }
 
     @seatsio.events.change_object_status(event.key, [props1, props2], 'status_foo')
     assert_equal(5, @seatsio.events.retrieve_object_info(key: event.key, label: 'GA1').num_booked)
@@ -90,22 +90,22 @@ class ChangeObjectStatusTest < SeatsioTestClient
   def test_change_object_status_with_extra_data
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
-    props1 = {:objectId => 'A-1', :extraData => {'foo': 'bar'}}
-    props2 = {:objectId => 'A-2', :extraData => {'foo': 'baz'}}
+    props1 = { :objectId => 'A-1', :extraData => { 'foo': 'bar' } }
+    props2 = { :objectId => 'A-2', :extraData => { 'foo': 'baz' } }
 
     @seatsio.events.change_object_status(event.key, [props1, props2], 'status_foo')
 
     object_info1 = @seatsio.events.retrieve_object_info key: event.key, label: 'A-1'
     object_info2 = @seatsio.events.retrieve_object_info key: event.key, label: 'A-2'
 
-    assert_equal({'foo' => 'bar'}, object_info1.extra_data)
-    assert_equal({'foo' => 'baz'}, object_info2.extra_data)
+    assert_equal({ 'foo' => 'bar' }, object_info1.extra_data)
+    assert_equal({ 'foo' => 'baz' }, object_info2.extra_data)
   end
 
   def test_keep_extra_data_true
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
-    extra_data = {'name' => 'John Doe'}
+    extra_data = { 'name' => 'John Doe' }
     @seatsio.events.update_extra_data key: event.key, object: 'A-1', extra_data: extra_data
 
     @seatsio.events.change_object_status(event.key, 'A-1', 'someStatus', keep_extra_data: true)
@@ -117,7 +117,7 @@ class ChangeObjectStatusTest < SeatsioTestClient
   def test_keep_extra_data_false
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
-    extra_data = {'name' => 'John Doe'}
+    extra_data = { 'name' => 'John Doe' }
     @seatsio.events.update_extra_data key: event.key, object: 'A-1', extra_data: extra_data
 
     @seatsio.events.change_object_status(event.key, 'A-1', 'someStatus', keep_extra_data: false)
@@ -129,7 +129,7 @@ class ChangeObjectStatusTest < SeatsioTestClient
   def test_no_keep_extra_data
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
-    extra_data = {'name' => 'John Doe'}
+    extra_data = { 'name' => 'John Doe' }
     @seatsio.events.update_extra_data key: event.key, object: 'A-1', extra_data: extra_data
 
     @seatsio.events.change_object_status(event.key, 'A-1', 'someStatus')
@@ -142,10 +142,10 @@ class ChangeObjectStatusTest < SeatsioTestClient
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
     @seatsio.events.update_channels key: event.key, channels: {
-        "channelKey1" => {"name" => "channel 1", "color" => "#FF0000", "index" => 1}
+      "channelKey1" => { "name" => "channel 1", "color" => "#FF0000", "index" => 1 }
     }
     @seatsio.events.assign_objects_to_channels key: event.key, channelConfig: {
-        "channelKey1" => ["A-1", "A-2"]
+      "channelKey1" => ["A-1", "A-2"]
     }
 
     @seatsio.events.change_object_status(event.key, 'A-1', "someStatus", channel_keys: ["channelKey1"])
@@ -158,10 +158,10 @@ class ChangeObjectStatusTest < SeatsioTestClient
     chart_key = create_test_chart
     event = @seatsio.events.create chart_key: chart_key
     @seatsio.events.update_channels key: event.key, channels: {
-        "channelKey1" => {"name" => "channel 1", "color" => "#FF0000", "index" => 1}
+      "channelKey1" => { "name" => "channel 1", "color" => "#FF0000", "index" => 1 }
     }
     @seatsio.events.assign_objects_to_channels key: event.key, channelConfig: {
-        "channelKey1" => ["A-1", "A-2"]
+      "channelKey1" => ["A-1", "A-2"]
     }
 
     @seatsio.events.change_object_status(event.key, 'A-1', "someStatus", ignore_channels: true)
@@ -173,11 +173,11 @@ class ChangeObjectStatusTest < SeatsioTestClient
   def test_ignore_social_distancing
     chart_key = create_test_chart
     rulesets = {
-        "ruleset" => {
-            "name" => "ruleset",
-            "fixedGroupLayout" => true,
-            "disabledSeats" => ["A-1"]
-        }
+      "ruleset" => {
+        "name" => "ruleset",
+        "fixedGroupLayout" => true,
+        "disabledSeats" => ["A-1"]
+      }
     }
     @seatsio.charts.save_social_distancing_rulesets(chart_key, rulesets)
     event = @seatsio.events.create chart_key: chart_key, social_distancing_ruleset_key: "ruleset"
@@ -186,5 +186,32 @@ class ChangeObjectStatusTest < SeatsioTestClient
 
     object_info = @seatsio.events.retrieve_object_info key: event.key, label: 'A-1'
     assert_equal("someStatus", object_info.status)
+  end
+
+  def test_allowed_previous_statuses
+    begin
+      chart_key = create_test_chart
+      event = @seatsio.events.create chart_key: chart_key
+
+      @seatsio.events.change_object_status(event.key, ['A-1'], 'foo', :allowed_previous_statuses => ['someOtherStatus'])
+      raise "Should have failed: expected SeatsioException"
+    rescue Seatsio::Exception::SeatsioException => e
+      assert_equal(400, e.message.code)
+      assert_match /ILLEGAL_STATUS_CHANGE/, e.message.body
+      assert_match /free is not in the list of allowed previous statuses/, e.message.body
+    end
+  end
+
+  def test_rejected_previous_statuses
+    begin
+      chart_key = create_test_chart
+      event = @seatsio.events.create chart_key: chart_key
+      @seatsio.events.change_object_status(event.key, ['A-1'], 'foo', :rejected_previous_statuses => ['free'])
+      raise "Should have failed: expected SeatsioException"
+    rescue Seatsio::Exception::SeatsioException => e
+      assert_equal(400, e.message.code)
+      assert_match /ILLEGAL_STATUS_CHANGE/, e.message.body
+      assert_match /free is in the list of rejected previous statuses/, e.message.body
+    end
   end
 end
