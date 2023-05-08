2595c893224b35e27edc944d47debd7a14f774b8
mroloux <matti@seats.io>
2023-05-08 12:31:52 +0200

    Added tryToPreventOrphanSeats flag when selecting best available objects (#74)

--- lib/seatsio/events/change_best_available_object_status_request.rb
+++ lib/seatsio/events/change_best_available_object_status_request.rb
@@ -1,9 +1,10 @@
-def create_change_best_available_object_status_request(number, status, categories, extra_data, ticket_types, hold_token, order_id, keep_extra_data, ignore_channels, channel_keys)
+def create_change_best_available_object_status_request(number, status, categories, extra_data, ticket_types, hold_token, order_id, keep_extra_data, ignore_channels, channel_keys, try_to_prevent_orphan_seats)
   result = {}
   best_available = {'number': number}
   best_available[:categories] = categories if categories != nil
   best_available[:extraData] = extra_data if extra_data != nil
   best_available[:ticketTypes] = ticket_types if ticket_types != nil
+  best_available[:tryToPreventOrphanSeats] = try_to_prevent_orphan_seats if try_to_prevent_orphan_seats != nil
   result[:status] = status
   result[:bestAvailable] = best_available
   result[:holdToken] = hold_token if hold_token != nil
