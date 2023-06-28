58d4b8d7cd3e293dbbb6f7aeb36cadde9859e4b7
Matti Roloux <matti@seats.io>
2023-06-28 09:22:48 +0200

    Don't release if nothing changed

--- release.mjs
+++ release.mjs
@@ -20,13 +20,14 @@
 $.verbose = false
 
 const versionToBump = getVersionToBump()
-const latestVersion = await fetchLatestReleasedVersionNumber()
+const latestReleaseTag = await fetchLatestReleasedVersionNumber()
+const latestVersion = removeLeadingV(latestReleaseTag)
 const nextVersion = await determineNextVersionNumber(latestVersion)
 
-await pullLastVersion()
-    .then(bumpVersionInFiles)
-    .then(commitAndPush)
-    .then(release)
+await assertChangesSinceRelease(latestReleaseTag)
+await bumpVersionInFiles()
+await commitAndPush()
+await release()
 
 function getVersionToBump() {
     if (!argv.v || !(argv.v === 'minor' || argv.v === 'major')) {
@@ -44,8 +45,7 @@ function removeLeadingV(tagName) {
 
 async function fetchLatestReleasedVersionNumber() {
     let result = await $`gh release view --json tagName`
-    let tagName = JSON.parse(result).tagName
-    return removeLeadingV(tagName)
+    return JSON.parse(result).tagName
 }
 
 async function determineNextVersionNumber(previous) {
@@ -70,11 +70,6 @@ async function replaceInFile(filename, latestVersion, nextVersion) {
         .then(() => gitAdd(filename))
 }
 
-async function pullLastVersion() {
-    await $`git checkout master`
-    await $`git pull origin master`
-}
-
 async function gitAdd(filename) {
     return await $`git add ${filename}`
 }
@@ -84,8 +79,24 @@ async function commitAndPush() {
     await $`git push origin master`
 }
 
+async function getCurrentCommitHash() {
+  return (await $`git rev-parse HEAD`).stdout.trim()
+}
+
+async function getCommitHashOfTag(tag) {
+  return (await $`git rev-list -n 1 ${tag}`).stdout.trim()
+}
+
+async function assertChangesSinceRelease(releaseTag) {
+  let masterCommitHash = await getCurrentCommitHash()
+  let releaseCommitHash = await getCommitHashOfTag(releaseTag)
+  if(masterCommitHash === releaseCommitHash) {
+    throw new Error("No changes on master since release tagged " + releaseTag)
+  }
+}
+
 async function release() {
-    const newTag = 'v' + nextVersion
+  const newTag = 'v' + nextVersion
     return await $`gh release create ${newTag} --generate-notes`.catch(error => {
         console.error('something went wrong while creating the release. Please revert the version change!')
         throw error
