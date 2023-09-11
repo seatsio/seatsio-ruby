34d2cbae4b78637f45c508e4cae3bcd4d3b295ec
Steve Chaloner <steve@seats.io>
2023-09-11 11:38:45 +0200

    Support draft reports for charts

--- lib/seatsio/chart_reports.rb
+++ lib/seatsio/chart_reports.rb
@@ -6,59 +6,67 @@ require 'cgi'
 
 module Seatsio
   class ChartReportsClient
+
     def initialize(http_client)
       @http_client = http_client
     end
 
-    def by_label(chart_key, book_whole_tables = nil)
-      fetch_chart_report('byLabel', chart_key, book_whole_tables)
+    def by_label(chart_key, book_whole_tables = nil, version = nil)
+      fetch_chart_report('byLabel', chart_key, book_whole_tables, version)
     end
 
-    def by_object_type(chart_key, book_whole_tables = nil)
-      fetch_chart_report('byObjectType', chart_key, book_whole_tables)
+    def by_object_type(chart_key, book_whole_tables = nil, version = nil)
+      fetch_chart_report('byObjectType', chart_key, book_whole_tables, version)
     end
 
-    def summary_by_object_type(chart_key, book_whole_tables = nil)
-      fetch_summary_report('byObjectType', chart_key, book_whole_tables)
+    def summary_by_object_type(chart_key, book_whole_tables = nil, version = nil)
+      fetch_summary_report('byObjectType', chart_key, book_whole_tables, version)
     end
 
-    def by_category_key(chart_key, book_whole_tables = nil)
-      fetch_chart_report('byCategoryKey', chart_key, book_whole_tables)
+    def by_category_key(chart_key, book_whole_tables = nil, version = nil)
+      fetch_chart_report('byCategoryKey', chart_key, book_whole_tables, version)
     end
 
-    def summary_by_category_key(chart_key, book_whole_tables = nil)
-      fetch_summary_report('byCategoryKey', chart_key, book_whole_tables)
+    def summary_by_category_key(chart_key, book_whole_tables = nil, version = nil)
+      fetch_summary_report('byCategoryKey', chart_key, book_whole_tables, version)
     end
 
-    def by_category_label(chart_key, book_whole_tables = nil)
-      fetch_chart_report('byCategoryLabel', chart_key, book_whole_tables)
+    def by_category_label(chart_key, book_whole_tables = nil, version = nil)
+      fetch_chart_report('byCategoryLabel', chart_key, book_whole_tables, version)
     end
 
-    def summary_by_category_label(chart_key, book_whole_tables = nil)
-      fetch_summary_report('byCategoryLabel', chart_key, book_whole_tables)
+    def summary_by_category_label(chart_key, book_whole_tables = nil, version = nil)
+      fetch_summary_report('byCategoryLabel', chart_key, book_whole_tables, version)
     end
 
-    def by_section(chart_key, book_whole_tables = nil)
-      fetch_chart_report('bySection', chart_key, book_whole_tables)
+    def by_section(chart_key, book_whole_tables = nil, version = nil)
+      fetch_chart_report('bySection', chart_key, book_whole_tables, version)
     end
 
-    def summary_by_section(event_key, book_whole_tables = nil)
-      fetch_summary_report('bySection', event_key, book_whole_tables)
+    def summary_by_section(event_key, book_whole_tables = nil, version = nil)
+      fetch_summary_report('bySection', event_key, book_whole_tables, version)
     end
 
     private
 
-    def fetch_chart_report(report_type, chart_key, book_whole_tables)
+    def fetch_chart_report(report_type, chart_key, book_whole_tables, version)
       params = book_whole_tables.nil? ? {} : { bookWholeTables: book_whole_tables }
+      unless version.nil?
+        params[:version] = version
+      end
       url = "reports/charts/#{chart_key}/#{report_type}"
       body = @http_client.get(url, params)
       ChartReport.new(body)
     end
 
-    def fetch_summary_report(report_type, event_key, book_whole_tables)
+    def fetch_summary_report(report_type, chart_key, book_whole_tables, version)
       params = book_whole_tables.nil? ? {} : { bookWholeTables: book_whole_tables }
-      url = "reports/charts/#{event_key}/#{report_type}/summary"
+      unless version.nil?
+        params[:version] = version
+      end
+      url = "reports/charts/#{chart_key}/#{report_type}/summary"
       @http_client.get(url, params)
     end
   end
+
 end
--- test/reports/charts/chart_reports_test.rb
+++ test/reports/charts/chart_reports_test.rb
@@ -4,143 +4,219 @@ require 'seatsio/domain'
 
 class ChartReportsTest < SeatsioTestClient
   def test_reportItemProperties
-    chart_key = create_test_chart
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key)
+      report = get_report.(chart_key)
 
-    assert_instance_of(Seatsio::ChartReport, report)
+      assert_instance_of(Seatsio::ChartReport, report)
 
-    report_item = report.items['A-1'][0]
-    assert_instance_of(Seatsio::ChartObjectInfo, report_item)
-    assert_equal('A-1', report_item.label)
-    assert_equal({'own' => {'label' => '1', 'type' => 'seat'}, 'parent' => {'label' => 'A', 'type' => 'row'}}, report_item.labels)
-    assert_equal({'own' => '1', 'parent' => 'A'}, report_item.ids)
-    assert_equal('Cat1', report_item.category_label)
-    assert_equal('9', report_item.category_key)
-    assert_equal('seat', report_item.object_type)
-    assert_nil(report_item.section)
-    assert_nil(report_item.entrance)
-    assert_nil(report_item.capacity)
-    assert_nil(report_item.left_neighbour)
-    assert_equal('A-2', report_item.right_neighbour)
-    assert_nil(report_item.book_as_a_whole)
-    assert_not_nil(report_item.distance_to_focal_point)
-    assert_not_nil(report_item.is_accessible)
-    assert_not_nil(report_item.is_companion_seat)
-    assert_not_nil(report_item.has_restricted_view)
+      report_item = report.items['A-1'][0]
+      assert_instance_of(Seatsio::ChartObjectInfo, report_item)
+      assert_equal('A-1', report_item.label)
+      assert_equal({'own' => {'label' => '1', 'type' => 'seat'}, 'parent' => {'label' => 'A', 'type' => 'row'}}, report_item.labels)
+      assert_equal({'own' => '1', 'parent' => 'A'}, report_item.ids)
+      assert_equal('Cat1', report_item.category_label)
+      assert_equal('9', report_item.category_key)
+      assert_equal('seat', report_item.object_type)
+      assert_nil(report_item.section)
+      assert_nil(report_item.entrance)
+      assert_nil(report_item.capacity)
+      assert_nil(report_item.left_neighbour)
+      assert_equal('A-2', report_item.right_neighbour)
+      assert_nil(report_item.book_as_a_whole)
+      assert_not_nil(report_item.distance_to_focal_point)
+      assert_not_nil(report_item.is_accessible)
+      assert_not_nil(report_item.is_companion_seat)
+      assert_not_nil(report_item.has_restricted_view)
+    end
   end
 
   def test_report_item_properties_for_GA
-    chart_key = create_test_chart
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key)
-    assert_instance_of(Seatsio::ChartReport, report)
+      report = get_report.(chart_key)
+      assert_instance_of(Seatsio::ChartReport, report)
 
-    report_item = report.items['GA1'][0]
-    assert_instance_of(Seatsio::ChartObjectInfo, report_item)
-    assert_equal('GA1', report_item.label)
-    assert_equal('generalAdmission', report_item.object_type)
-    assert_equal('Cat1', report_item.category_label)
-    assert_equal('9', report_item.category_key)
-    assert_nil(report_item.section)
-    assert_nil(report_item.entrance)
-    assert_equal(100, report_item.capacity)
-    assert_false(report_item.book_as_a_whole)
+      report_item = report.items['GA1'][0]
+      assert_instance_of(Seatsio::ChartObjectInfo, report_item)
+      assert_equal('GA1', report_item.label)
+      assert_equal('generalAdmission', report_item.object_type)
+      assert_equal('Cat1', report_item.category_label)
+      assert_equal('9', report_item.category_key)
+      assert_nil(report_item.section)
+      assert_nil(report_item.entrance)
+      assert_equal(100, report_item.capacity)
+      assert_false(report_item.book_as_a_whole)
+    end
   end
 
   def test_report_item_properties_for_table
-    chart_key = create_test_chart_with_tables
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'true') }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'true', 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart_with_tables
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key, 'true')
+      report = get_report.(chart_key)
 
-    report_item = report.items['T1'][0]
-    assert_equal(6, report_item.num_seats)
-    assert_equal(false, report_item.book_as_a_whole)
+      report_item = report.items['T1'][0]
+      assert_equal(6, report_item.num_seats)
+      assert_equal(false, report_item.book_as_a_whole)
+    end
   end
 
   def test_by_label
-    chart_key = create_test_chart
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key)
+      report = get_report.(chart_key)
 
-    assert_instance_of(Seatsio::ChartReport, report)
-    assert_equal(1, report.items['A-1'].length)
-    assert_equal(1, report.items['A-2'].length)
+      assert_instance_of(Seatsio::ChartReport, report)
+      assert_equal(1, report.items['A-1'].length)
+      assert_equal(1, report.items['A-2'].length)
+    end
   end
 
   def test_by_label_book_whole_tables_nil
-    chart_key = create_test_chart_with_tables
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart_with_tables
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key)
+      report = get_report.(chart_key)
 
-    assert_instance_of(Seatsio::ChartReport, report)
-    assert_equal(14, report.items.length)
+      assert_instance_of(Seatsio::ChartReport, report)
+      assert_equal(14, report.items.length)
+    end
   end
 
   def test_by_label_book_whole_tables_chart
-    chart_key = create_test_chart_with_tables
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'chart') }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'chart', 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart_with_tables
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key, 'chart')
+      report = get_report.(chart_key)
 
-    assert_instance_of(Seatsio::ChartReport, report)
-    assert_equal(7, report.items.length)
+      assert_instance_of(Seatsio::ChartReport, report)
+      assert_equal(7, report.items.length)
+    end
   end
 
   def test_by_label_book_whole_tables_true
-    chart_key = create_test_chart_with_tables
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'true') }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'true', 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart_with_tables
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key, 'true')
+      report = get_report.(chart_key)
 
-    assert_instance_of(Seatsio::ChartReport, report)
-    assert_equal(2, report.items.length)
+      assert_instance_of(Seatsio::ChartReport, report)
+      assert_equal(2, report.items.length)
+    end
   end
 
   def test_by_label_book_whole_tables_false
-    chart_key = create_test_chart_with_tables
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'false') }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_label(chart_key, 'false', 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart_with_tables
+      update_chart.(chart_key)
 
-    report = @seatsio.chart_reports.by_label(chart_key, 'false')
+      report = get_report.(chart_key)
 
-    assert_instance_of(Seatsio::ChartReport, report)
-    assert_equal(12, report.items.length)
+      assert_instance_of(Seatsio::ChartReport, report)
+      assert_equal(12, report.items.length)
+    end
   end
 
   def test_by_object_type
-    chart_key = create_test_chart
-    report = @seatsio.chart_reports.by_object_type(chart_key)
-    assert_equal(4, report.items.length)
-    assert_equal(32, report.items['seat'].length)
-    assert_equal(2, report.items['generalAdmission'].length)
-    assert_equal(0, report.items['booth'].length)
-    assert_equal(0, report.items['table'].length)
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_object_type(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_object_type(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart
+      update_chart.(chart_key)
+
+      report = get_report.(chart_key)
+      assert_equal(4, report.items.length)
+      assert_equal(32, report.items['seat'].length)
+      assert_equal(2, report.items['generalAdmission'].length)
+      assert_equal(0, report.items['booth'].length)
+      assert_equal(0, report.items['table'].length)
+    end
   end
 
   def test_by_category_key
-    chart_key = create_test_chart
-    report = @seatsio.chart_reports.by_category_key(chart_key)
-    assert_equal(4, report.items.length)
-    assert_equal(17, report.items['9'].length)
-    assert_equal(17, report.items['10'].length)
-    assert_equal(0, report.items['string11'].length)
-    assert_equal(0, report.items['NO_CATEGORY'].length)
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_category_key(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_category_key(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart
+      update_chart.(chart_key)
+
+      report = get_report.(chart_key)
+      assert_equal(4, report.items.length)
+      assert_equal(17, report.items['9'].length)
+      assert_equal(17, report.items['10'].length)
+      assert_equal(0, report.items['string11'].length)
+      assert_equal(0, report.items['NO_CATEGORY'].length)
+    end
   end
 
   def test_by_category_label
-    chart_key = create_test_chart
-    report = @seatsio.chart_reports.by_category_label(chart_key)
-    assert_equal(4, report.items.length)
-    assert_equal(17, report.items['Cat1'].length)
-    assert_equal(17, report.items['Cat2'].length)
-    assert_equal(0, report.items['Cat3'].length)
-    assert_equal(0, report.items['NO_CATEGORY'].length)
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_category_label(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_category_label(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart
+      update_chart.(chart_key)
+
+      report = get_report.(chart_key)
+      assert_equal(4, report.items.length)
+      assert_equal(17, report.items['Cat1'].length)
+      assert_equal(17, report.items['Cat2'].length)
+      assert_equal(0, report.items['Cat3'].length)
+      assert_equal(0, report.items['NO_CATEGORY'].length)
+    end
   end
 
   def test_by_section
-    chart_key = create_test_chart_with_sections
-    report = @seatsio.chart_reports.by_section(chart_key)
-    assert_equal(3, report.items.length)
-    assert_equal(36, report.items['Section A'].length)
-    assert_equal(35, report.items['Section B'].length)
-    assert_equal(0, report.items['NO_SECTION'].length)
+    [
+      [-> (_) { }, -> (chart_key) { @seatsio.chart_reports.by_section(chart_key) }],
+      [-> (chart_key) { create_draft_version chart_key }, -> (chart_key) { @seatsio.chart_reports.by_section(chart_key, nil, 'draft') }]
+    ].each do |update_chart, get_report|
+      chart_key = create_test_chart_with_sections
+      update_chart.(chart_key)
+
+      report = get_report.(chart_key)
+      assert_equal(3, report.items.length)
+      assert_equal(36, report.items['Section A'].length)
+      assert_equal(35, report.items['Section B'].length)
+      assert_equal(0, report.items['NO_SECTION'].length)
+    end
   end
 
   def test_with_extra_data
@@ -155,5 +231,10 @@ class ChartReportsTest < SeatsioTestClient
     report = @seatsio.event_reports.by_label(event1.key)
 
     assert_equal(extra_data, report.items['A-1'][0].extra_data)
+  end
+
+  private def create_draft_version(chart_key)
+    @seatsio.events.create chart_key: chart_key
+    @seatsio.charts.update key: chart_key, new_name: 'foo'
   end
 end
