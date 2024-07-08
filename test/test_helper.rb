e0f0fb014372d0585935cb356184d9dec5d8fc21
mroloux <matti@seats.io>
2024-07-08 11:02:54 +0200

    Use different parallel test runner to avoid ugly stack traces (#112)
    * Use different parallel test runner to avoid ugly stack traces
    
    * Fix build

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require 'webmock/minitest'
 require "seatsio"
 require "minitest/autorun"
-require "minitest/parallel_fork"
 require 'custom_assertions'
 require 'seatsio_test_client'
 
