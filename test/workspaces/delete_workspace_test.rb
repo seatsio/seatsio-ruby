4aacec7ac47c7ff9c21a1105c703f34581d8d8af
Steve Chaloner <schaloner@users.noreply.github.com>
2025-06-17 14:16:37 +0200

    Delete workspaces (#147)
    * Delete workspaces
    
    This PR exposes workspace deletion for inactive workspaces.
    
    * Removed unnecessary test
    
    ---------
    
    Co-authored-by: Steve Chaloner <steve@seats.io>

---
+++ test/workspaces/delete_workspace_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+require 'util'
+
+class DeleteWorkspaceTest < SeatsioTestClient
+  def test_delete_inactive_workspace
+    workspace = @seatsio.workspaces.create name: 'a ws'
+    @seatsio.workspaces.deactivate key: workspace.key
+
+    @seatsio.workspaces.delete key: workspace.key
+
+    assert_raises(Seatsio::Exception::NotFoundException) do
+      @seatsio.workspaces.retrieve key: workspace.key
+    end
+  end
+end
