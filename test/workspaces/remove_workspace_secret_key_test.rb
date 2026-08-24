12f448561f3f98a9a34e15dd1560289d4eb80f09
Steve Chaloner <schaloner@users.noreply.github.com>
2026-08-24 13:29:23 +0200

    Support add/remove secret keys (#172)
    * Support add/remove secret keys
    
    * Bump node version for release workflow
    
    ---------
    
    Co-authored-by: Steve Chaloner <steve@seats.io>

---
+++ test/workspaces/remove_workspace_secret_key_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+require 'util'
+
+class AddWorkspaceSecretKeyTest < SeatsioTestClient
+  def test_add_secret_key
+    workspace = @seatsio.workspaces.create name: 'a ws'
+
+    new_secret_key = @seatsio.workspaces.add_secret_key key: workspace.key
+
+    assert_not_blank(new_secret_key)
+    assert_not_equal(new_secret_key, workspace.secret_key)
+    retrieved_workspace = @seatsio.workspaces.retrieve key: workspace.key
+    assert_equal([workspace.secret_key, new_secret_key], retrieved_workspace.secret_keys)
+
+    @seatsio.workspaces.remove_secret_key key: workspace.key, secret_key_to_remove: workspace.secret_key
+
+    final_state_workspace = @seatsio.workspaces.retrieve key: workspace.key
+    assert_equal([new_secret_key], final_state_workspace.secret_keys)
+  end
+end
